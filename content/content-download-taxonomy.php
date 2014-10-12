733685bbb36ca365ea64bc1dc19ea3c9c2fe4eee
Sean Davis <sdavis2702@gmail.com>
2014-10-12 18:57:33 -0500

    fix pagination bug on download cats & tags

--- content/content-download-taxonomy.php
+++ content/content-download-taxonomy.php
@@ -55,7 +55,7 @@
 			echo paginate_links( array(
 				'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 				'format' => '?paged=%#%',
-				'current' => max( 1, $current_page ),
+				'current' => max( 1, get_query_var( 'paged' ) ),
 				'total' => $wp_query->max_num_pages,
 				'prev_text' => '<i class="fa fa-arrow-circle-left"></i> Previous',
 				'next_text' => 'Next <i class="fa fa-arrow-circle-right"></i>'
